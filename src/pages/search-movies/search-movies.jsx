b558eba895ffac5e11244c977458179cb6828916
AnahitKhechumyan <70436905+AnahitKhechumyan@users.noreply.github.com>
2025-02-12 23:14:35 +0400

    add useReducer

--- src/pages/search-movies/search-movies.jsx
+++ src/pages/search-movies/search-movies.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useReducer, useEffect, useRef} from "react";
 import axios from 'axios';
 import { Table } from "../../components/table/table";
 import { Modal } from "../../components/modal/modal";
@@ -8,22 +8,42 @@ import { APP_TITLE } from "../../utils/constants";
 import { getAppTitleByMovie } from "../../utils/helpers";
 import {Pagination} from "../../components/pagination";
  
+const initialState = {
+  data: [],
+  open: false,
+  selectedMovie: null,
+};
 
+const searchMovieReducer = (state, action) =>{
+  switch (action.type) {
+    case 'SET_DATA'://{ type: "SET_DATA", payload: [] }
+      return {...state, data: action.payload};
+    case 'SET_MODAL_OPEN': // { type: "SET_MODAL_OPEN", payload: false }
+      return {...state, open: action.payload};
+    case 'SET_SELECTED_MOVIE': // { type: "SET_MODAL_OPEN", payload: false }
+      return {
+        ...state,
+        open: action.payload.open,
+        selectedMovie: action.payload.selectedMovie,
+      };
+      default:
+        break;
+  }
+};
 export const SearchMovies = ({ searchQuery }) => {
-  const [data, setData] = useState([]);
-  const [open, setModalOpen] = useState(false);
-  const [timeoutId, setTimeoutId] = useState(null);
-  const [selectedMovie, setSelectedMovie] = useState(null);
-  const [init, setInit] = useState(false);
+   
+  const [state, dispatch] = useReducer(searchMovieReducer, initialState);
+
+  const timeoutIdRef = useRef(null);
+  
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [moviesPerPage] = useState(5);
 
-
   useEffect(() => {
     axios.get('data.json')
         .then(res => {
-                setData(res.data);
+          dispatch({type: 'SET_DATA', payload: res.data.results});
                 setLoading(false);
             })
             .catch(() => {
@@ -31,56 +51,59 @@ export const SearchMovies = ({ searchQuery }) => {
             })
 }, [])
 
-
 const indexOfLastMovies = currentPage * moviesPerPage;
 const indexOfFirstMovies = indexOfLastMovies - moviesPerPage;
-const currentMovies = data.slice(indexOfFirstMovies, indexOfLastMovies);
-const nPages = Math.ceil(data.length / moviesPerPage);
+const currentMovies = state.data.slice(indexOfFirstMovies, indexOfLastMovies);
+const nPages = Math.ceil(state.data.length / moviesPerPage);
  
   const fetchMovies = async () => {
     const response = await omdbApi.fetchMoviesBySearch(searchQuery || "");
 
     if (response.success) {
-      setData(response.data.Search || []);
+      dispatch({ type: 'SET_DATA', payload: response.data.Search || [] });
     }
   };
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
 
-    console.log(window.location.search);
-
     const movieId = urlParams.get("movieId");
     const title = urlParams.get("title");
     const year = urlParams.get("year");
 
     if (movieId && title && year) {
-      setModalOpen(true);
-      setSelectedMovie({ imdbID: movieId, Title: title, Year: year });
+      dispatch({
+        type: "SET_SELECTED_MOVIE",
+        payload: { 
+          open:true,
+          selectedMovie:{imdbID: movieId, Title: title, Year: year},
+        },
+      });
        document.title = getAppTitleByMovie(title, year);
     }
   }, []);
 
   useEffect(() => {
     fetchMovies();
-    setInit(true);
   }, []);
 
   useEffect(() => {
-    if (!init) return;
-
-    clearTimeout(timeoutId);
-
+    clearTimeout(timeoutIdRef.current);
     const toId = setTimeout(() => {
       fetchMovies();
-    }, 1000);
+    }, [1000]);
 
-    setTimeoutId(toId);
+    timeoutIdRef.current = toId;
   }, [searchQuery]);
 
   const handleRowClick = (row) => {
-    setModalOpen(true);
-    setSelectedMovie(row);
+    dispatch({
+      type: "SET_SELECTED_MOVIE",
+      payload: {
+        open: true,
+        selectedMovie: row,
+        },
+    });
 
     document.title =getAppTitleByMovie(row.Title,row.Year);
 
@@ -92,7 +115,10 @@ const nPages = Math.ceil(data.length / moviesPerPage);
   };
 
   const handleCloseModal = () => {
-    setModalOpen(false);
+    dispatch({
+      type: "SET_MODAL_OPEN",
+      payload:false, 
+       });
     window.history.pushState("", "", "/");
     document.title = APP_TITLE;
 
@@ -107,11 +133,11 @@ const nPages = Math.ceil(data.length / moviesPerPage);
                 setCurrentPage={setCurrentPage}
        />
       <Modal
-        open={open}
+        open={state.open}
         onClose={handleCloseModal}
-        title={getAppTitleByMovie(selectedMovie?.Title, selectedMovie?.Year)}
+        title={getAppTitleByMovie(state.selectedMovie?.Title, state.selectedMovie?.Year)}
       >
-        <MovieDetails id={selectedMovie?.imdbID} />
+        <MovieDetails id={state.selectedMovie?.imdbID} />
       </Modal>
        
     </div>
